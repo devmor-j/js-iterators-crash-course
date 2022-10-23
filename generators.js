60f87905f7c37bf70f3fe698ee03dc57c993fc63
Morteza Jamshidi <devmor.j@gmail.com>
2022-10-23 21:08:51 +0330

    write a simple yield example

--- generators.js
+++ generators.js
@@ -1,6 +1,6 @@
 // The Generator object is returned by a generator function
 // and it conforms to both the iterable protocol
-// and the iterator protocol.
+// and the iterator protocol
 
 function* loopCounterGenerator(loopSize, maxIteration = loopSize * 2) {
   let counter = 0;
@@ -19,3 +19,29 @@ const loopCounter = loopCounterGenerator(3);
 // for (const c of loopCounter) {
 //   console.log(c);
 // }
+
+// The yield* expression is used to delegate to
+// another generator or iterable object
+
+function* randomBoolean() {
+  let chances = 3;
+  while (chances > 0) {
+    yield Math.random() < 0.5 ? false : true;
+    chances--;
+  }
+  return true;
+}
+
+function* truthOrFalse() {
+  while (true) {
+    console.log(yield* randomBoolean());
+  }
+}
+
+const booleanGame = truthOrFalse();
+
+console.log(booleanGame.next().value);
+console.log(booleanGame.next().value);
+console.log(booleanGame.next().value);
+console.log(booleanGame.next().value);
+console.log(booleanGame.next().value);
