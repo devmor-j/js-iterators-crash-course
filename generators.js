a5d19895ae673e8a7959a74fce7bd7de88f9628b
Morteza Jamshidi <devmor.j@gmail.com>
2022-10-23 23:15:53 +0330

    add game example for generator

--- generators.js
+++ generators.js
@@ -2,6 +2,9 @@
 // and it conforms to both the iterable protocol
 // and the iterator protocol
 
+// iterable protocol refers to [Symbol.iterator]
+// iterator protocol refers to { next() {} }
+
 function* loopCounterGenerator(loopSize, maxIteration = loopSize * 2) {
   let counter = 0;
   let iteration = 0;
@@ -20,28 +23,89 @@ const loopCounter = loopCounterGenerator(3);
 //   console.log(c);
 // }
 
-// The yield* expression is used to delegate to
+// --------------------------------------------------------
+
+// you can call another generator function using yield*
+// In other words yield* expression is used to delegate to
 // another generator or iterable object
 
-function* randomBoolean() {
-  let chances = 3;
-  while (chances > 0) {
-    yield Math.random() < 0.5 ? false : true;
-    chances--;
-  }
-  return true;
+function* gen1() {
+  yield 2;
+  yield 3;
 }
 
-function* truthOrFalse() {
-  while (true) {
-    console.log(yield* randomBoolean());
+function* gen2() {
+  yield 1;
+  yield* gen1();
+  yield 4;
+}
+
+// for (const i of gen2()) {
+//   console.log(i); // 1, 2, 3, 4
+// }
+
+// --------------------------------------------------------
+
+// Example: Fight Club Game
+
+const GameDifficulty = {
+  EASY: 0.25,
+  NORMAL: 0.5,
+  HARD: 0.75,
+};
+
+function fakeDelay(duration = 300) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(true);
+    }, duration);
+  });
+}
+
+function randomBoolean(truthChance = 0.5) {
+  return Math.random() < truthChance ? false : true;
+}
+
+function attack({ strength = 0.5 } = {}) {
+  return Math.round(10 + Math.random() * 40 * strength);
+}
+
+async function* fightClubGenerator({ difficulty }) {
+  let playerHealth = 75 + (1 - difficulty) * 100;
+  let enemyHealth = 125 - (1 - difficulty) * 100;
+
+  console.log("\nFIGHT!\n");
+  console.log(`You 🧡 ${playerHealth} --- Enemy 💙 ${enemyHealth}`);
+
+  while (playerHealth > 0 && enemyHealth > 0) {
+    await fakeDelay(300 + Math.random() * 600);
+
+    const isPlayerHit = randomBoolean(difficulty);
+
+    if (isPlayerHit) {
+      playerHealth -= attack({ strength: difficulty });
+    } else {
+      enemyHealth -= attack({ strength: 1 - difficulty });
+    }
+
+    if (playerHealth <= 0) {
+      yield "Game Over! 💀";
+      return;
+    } else if (enemyHealth <= 0) {
+      yield "You Won! 🌟⚔🌟";
+      return;
+    }
+
+    yield `You 🧡 ${playerHealth} --- Enemy 💙 ${enemyHealth}`;
   }
 }
 
-const booleanGame = truthOrFalse();
+const playGame = async () => {
+  const fightClub = fightClubGenerator({ difficulty: GameDifficulty.NORMAL });
 
-console.log(booleanGame.next().value);
-console.log(booleanGame.next().value);
-console.log(booleanGame.next().value);
-console.log(booleanGame.next().value);
-console.log(booleanGame.next().value);
+  for await (const fight of fightClub) {
+    console.log(fight);
+  }
+};
+
+// playGame();
